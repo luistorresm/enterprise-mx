a6091cac8ccc4fa79d89e32d8e58f18164403905
None <None>
2016-12-31 21:41:37 +0000

    basic-demo

---
+++ l10n_mx_enterprise/__manifest__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Odoo Mexican Localization Demo Data Load",
+    "description": "Dummy and temporal module to configure runbot "
+    "with basic demo info",
+    "version": "1",
+    "depends": ["l10n_mx_edi"],
+    "demo": ["demo/global.xml"],
+    "installable": True,
+}
