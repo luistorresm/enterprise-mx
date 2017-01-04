c4ab3033c2d734607880f4a993fd625382fb3cbd
Moisés López <moylop260@vauxoo.com>
2017-01-04 17:03:15 +0000

    Revert "Add depends to l10n_mx and remove company_country"
    This reverts commit 9750294f0df7aa9e117d218d2d5b13bad270cf4f.

--- l10n_mx_enterprise/__manifest__.py
+++ l10n_mx_enterprise/__manifest__.py
@@ -6,7 +6,7 @@
     "description": "Dummy and temporal module to configure runbot "
     "with basic demo info",
     "version": "1",
-    "depends": ["l10n_mx_edi", "l10n_mx"],
+    "depends": ["l10n_mx_edi"],
     "demo": ["demo/global.xml"],
     "installable": True,
 }
